209e05ffeb36ee38c3b89869da36a18584c46056
Debi Prasad Ratha <dpratha96@gmail.com>
2021-08-19 17:50:50 +0530

    Update end.py

--- end.py
+++ end.py
@@ -1,26 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
-
-driver = webdriver.Chrome(executable_path="C:\\bin\\chromedriver.exe")
-driver.get("https://rahulshettyacademy.com/angularpractice/")
-driver.find_element_by_css_selector("a[href*='shop']").click()
-products = driver.find_elements_by_xpath("//div[@class='card h-100']")
-
-for product in products:
-    productname = product.find_element_by_xpath("div/h4/a").text
-    if productname == "Blackberry":
-        product.find_element_by_xpath("div/button").click()
-driver.find_element_by_css_selector("a[class*='btn-primary']").click()
-driver.find_element_by_xpath("//button[@class='btn btn-success']").click()
-driver.find_element_by_id("country").send_keys("ind")
-wait = WebDriverWait(driver, 7)
-wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "India")))
-driver.find_element_by_link_text("India").click()
-driver.find_element_by_xpath("//div[@class='checkbox checkbox-primary']").click()
-driver.find_element_by_css_selector("[type='submit']").click()
-successText = driver.find_element_by_class_name("alert-success").text
-assert "Success! Thank you! Your order will be delivered in next few weeks :-)."
-driver.get_screenshot_as_file("screen.png")
+def simple_interest(p,t,r):
+    print('The principal is', p)
+    print('The time period is', t)
+    print('The rate of interest is',r)
+      
+    si = (p * t * r)/100
+      
+    print('The Simple Interest is', si)
+    return si
+      
+# Driver code
+simple_interest(8, 6, 8)
